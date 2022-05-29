c61d3ebd8f6d933a8c5a8b83b3486823dcb4586b
ashish <ashishkhms@gmail.com>
2022-05-30 00:10:24 +0530

    updated CORS

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(express.json());
 const connectDB = require('./config/db');
 connectDB();
+
+//CORS
+var cors = require('cors');
+var corsOptions = {
+    origin: 'https://ashishkhms.github.io/',
+    optionsSuccessStatus: 200,
+}
+app.use(cors(corsOptions));
 
 //template engine 
 app.set('views', path.join(__dirname, '/views'));
